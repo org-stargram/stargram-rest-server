d8021bca6c634e9f98c6c88dc8e194002a6af881
ChulWoo Kim <cwoo3511@naver.com>
2019-08-10 15:21:43 +0900

    First commit

---
+++ src/main/java/com/web/StargramRestServerApplication.java
@@ -0,0 +1,43 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@SpringBootApplication
+public class StargramRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StargramRestServerApplication.class, args);
+	}
+
+	@Configuration
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
+	@EnableWebSecurity
+	static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			CorsConfiguration configuration = new CorsConfiguration();
+			configuration.addAllowedOrigin("*");
+			configuration.addAllowedMethod("*");
+			configuration.addAllowedHeader("*");
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			source.registerCorsConfiguration("/**", configuration);
+
+			http.httpBasic()
+					.and().authorizeRequests()
+					//.antMatchers(HttpMethod.POST, "/Boards/**").hasRole("ADMIN")
+					.anyRequest().permitAll()
+					.and().cors().configurationSource(source)
+					.and().csrf().disable();
+		}
+	}
+}
---
+++ src/main/java/com/web/domain/User.java
@@ -0,0 +1,59 @@
+package com.web.domain;
+
+
+import com.web.domain.enums.SocialType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Created by KimYJ on 2017-07-12.
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+@Table
+public class User implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue
+    private Long idx;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @Column
+    private String pincipal;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @Builder
+    public User(String name, String password, String email, String pincipal, SocialType socialType, LocalDateTime createdDate, LocalDateTime updatedDate) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.pincipal = pincipal;
+        this.socialType = socialType;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+}
