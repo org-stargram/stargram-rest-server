8534d01d8ec8f404e6a6bd8c52832e260f648c26
ChulWoo Kim <cwoo3511@naver.com>
2019-08-13 21:53:05 +0900

    First commit - 4

--- src/main/java/com/web/controller/AccountRestController.java
+++ src/main/java/com/web/controller/AccountRestController.java
@@ -1,25 +1,15 @@
 package com.web.controller;
 
-import com.web.domain.Board;
 import com.web.domain.User;
-import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.web.domain.enums.SocialType.DEFAULT;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/account")
